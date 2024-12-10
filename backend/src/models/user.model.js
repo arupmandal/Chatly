7dc8a21ffa3bda54669cd3d3ced88eb3e587aba3
Arup Mandal <110271890+cyberboyarup@users.noreply.github.com>
2024-12-10 12:39:59 +0530

    Arup Mandal

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: [true, "Full name is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
